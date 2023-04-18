48be6114ba2508968fef3198aa68135f56627e61
Martin Vrba <Martin.Vrba@gmx.com>
2023-04-18 10:15:19 +0200

    Update Payment model

--- backend/pmb/admin.py
+++ backend/pmb/admin.py
@@ -3,17 +3,18 @@ from .models import Payment
 
 class PaymentAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
-        'iban',
         'amount',
-        'due_date',
-        'variable_symbol',
-        'specific_symbol',
-        'constant_symbol',
-        'payment_note',
-        'beneficiary_name',
         'beneficiary_address1',
         'beneficiary_address2',
+        'beneficiary_name',
+        'constant_symbol',
+        'due_date',
+        'iban',
+        'id',
+        'payment_note',
+        'specific_symbol',
+        'timestamp',
+        'variable_symbol',
     )
 
 # Register your models here.
--- backend/pmb/models.py
+++ backend/pmb/models.py
@@ -5,14 +5,15 @@ from uuid import uuid1
 # Create your models here.
 
 class Payment(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid1, editable=False)
-    iban = models.CharField(max_length=34)
     amount = models.FloatField()
-    due_date = models.DateField(default=date.today)
-    variable_symbol = models.CharField(default='', max_length=10)
-    specific_symbol = models.CharField(default='', max_length=10)
-    constant_symbol = models.CharField(default='', max_length=4)
-    payment_note = models.CharField(default='', max_length=99)
-    beneficiary_name = models.CharField(default='', max_length=99)
     beneficiary_address1 = models.CharField(default='', max_length=99)
     beneficiary_address2 = models.CharField(default='', max_length=99)
+    beneficiary_name = models.CharField(default='', max_length=99)
+    constant_symbol = models.CharField(default='', max_length=4)
+    due_date = models.DateField(default=date.today)
+    iban = models.CharField(max_length=34)
+    id = models.UUIDField(default=uuid1, editable=False, primary_key=True)
+    payment_note = models.CharField(default='', max_length=99)
+    specific_symbol = models.CharField(default='', max_length=10)
+    timestamp = models.DateTimeField(auto_now_add=True, editable=False)
+    variable_symbol = models.CharField(default='', max_length=10)
